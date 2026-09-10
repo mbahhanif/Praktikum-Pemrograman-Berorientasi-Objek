e751d4b0a546b452233bb7d4db8b4c5b874dd0bd
Mbah Bejo <hanifraharjo07@gmail.com>
2026-09-10 16:41:43 +0700

    Implement animal objects with methods in Main.java
    Added functionality to create animal objects with attributes and methods.

---
+++ kelas-dan-objek/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Hewan kucing = new Hewan();
+        kucing.nama = "Hachimi";
+        kucing.umur = 3;
+
+        kucing.suara();
+        kucing.info();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Objek Kucing: Hachimi, umur 3, suaranya "Miww Miaw"
+        Hewan kucing = new Hewan("Hachimi", 3, "Miww Miaw");
+        kucing.info();
+        kucing.bersuara();
+        kucing.berlari();
+
+        System.out.println(); // Spasi biar rapi di terminal
+
+        // Objek Anjing: Kiko, umur 2, suaranya "Auuu"
+        Hewan anjing = new Hewan("Kiko", 2, "Auuu");
+        anjing.info();
+        anjing.bersuara();
+        anjing.berlari();
+    }
+}
