34cd52d3ac745f44ffa4f58a4889dc63515888e6
Mbah Bejo <hanifraharjo07@gmail.com>
2026-09-16 10:32:54 +0700

    Delete Mobil and MainPraktikum classes from Kendaraan.java
    Removed the Mobil and MainPraktikum classes along with their methods.

--- enkapsulasi-dan-akses-modifer/src /praktikum4/Kendaraan.java
+++ enkapsulasi-dan-akses-modifer/src /praktikum4/Kendaraan.java
@@ -30,34 +30,6 @@ public class Kendaraan {
         System.out.println("Kecepatan Maksimum : " + kecepatanMaks + " km/h");
         System.out.println("Jenis Mesin        : " + jenisMesin);
     }
-} 
-
-public class Mobil extends Kendaraan {
-    private int jumlahPintu;
-
-    // Constructor memanggil constructor induk pakai super()
-    public Mobil(String nama, int kecepatanMaks, String jenisMesin, int jumlahPintu) {
-        super(nama, kecepatanMaks, jenisMesin);
-        this.jumlahPintu = jumlahPintu;
-    }
-
-    // Method menampilkan info spesifik mobil
-    public void tampilkanInfoMobil() {
-        // kecepatanMaks bisa diakses langsung karena modifier-nya protected!
-        System.out.println("Kecepatan Maks Mobil : " + kecepatanMaks + " km/h");
-        System.out.println("Jumlah Pintu         : " + jumlahPintu);
-    }
-} 
-
-public class MainPraktikum {
-    public static void main(String[] args) {
-        // Inisialisasi objek mobil menggunakan data Honda Mobilio RS (Tahun 2021)
-        Mobil mobilSaya = new Mobil("Honda Mobilio RS 2021", 160, "1.5L SOHC i-VTEC", 5);
-
-        System.out.println("=== INFO KENDARAAN ===");
-        mobilSaya.tampilkanInfoKendaraan();
-
-        System.out.println("\n=== INFO KHUSUS MOBIL ===");
-        mobilSaya.tampilkanInfoMobil();
-    }
 }
+
+
